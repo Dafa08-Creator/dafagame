604c0591b008affe0cb899f48fa3ed1fc7c8760b
Dafa08-Creator <dafaamarda4@gmail.com>
2024-08-11 14:15:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const KELAS_X = 'x';
+const KELAS_O = 'o';
+const KOMBINASI_MENANG = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+];
+const elemenSel = document.querySelectorAll('[data-sel]');
+const papan = document.getElementById('papan');
+const elemenPesanMenang = document.getElementById('pesanMenang');
+const teksPesanMenang = document.querySelector('[data-pesan-menang-teks]');
+const tombolUlangi = document.getElementById('tombolUlangi');
+let giliranO;
+
+mulaiPermainan();
+
+tombolUlangi.addEventListener('click', mulaiPermainan);
+
+function mulaiPermainan() {
+	giliranO = false;
+	elemenSel.forEach(sel => {
+		sel.classList.remove(KELAS_X);
+		sel.classList.remove(KELAS_O);
+		sel.removeEventListener('click', handleClick);
+		sel.addEventListener('click', handleClick, { once: true });
+	});
+	setPapanHoverClass();
+	elemenPesanMenang.classList.remove('show');
+}
+
+function handleClick(e) {
+	const sel = e.target;
+	const kelasSaatIni = giliranO ? KELAS_O : KELAS_X;
+	tempatkanTanda(sel, kelasSaatIni);
+	if (cekMenang(kelasSaatIni)) {
+		akhirPermainan(false);
+	} else if (adalahSeri()) {
+		akhirPermainan(true);
+	} else {
+		gantiGiliran();
+		setPapanHoverClass();
+	}
+}
+
+function akhirPermainan(seri) {
+	if (seri) {
+		teksPesanMenang.innerText = 'Seri!';
+	} else {
+		teksPesanMenang.innerText = `${giliranO ? "O" : "X"} Menang!`;
+	}
+	elemenPesanMenang.classList.add('show');
+}
+
+function adalahSeri() {
+	return [...elemenSel].every(sel => {
+		return sel.classList.contains(KELAS_X) || sel.classList.contains(KELAS_O);
+	});
+}
+
+function tempatkanTanda(sel, kelasSaatIni) {
+	sel.classList.add(kelasSaatIni);
+}
+
+function gantiGiliran() {
+	giliranO = !giliranO;
+}
+
+function setPapanHoverClass() {
+	papan.classList.remove(KELAS_X);
+	papan.classList.remove(KELAS_O);
+	if (giliranO) {
+		papan.classList.add(KELAS_O);
+	} else {
+		papan.classList.add(KELAS_X);
+	}
+}
+
+function cekMenang(kelasSaatIni) {
+	return KOMBINASI_MENANG.some(kombinasi => {
+		return kombinasi.every(index => {
+			return elemenSel[index].classList.contains(kelasSaatIni);
+		});
+	});
+}
